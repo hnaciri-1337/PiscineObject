39a944bc207df8e0d846446d61ed60c73a71e831
Haitham Naciri ben saghir <hnaciri-@c3r7p10.1337.ma>
2023-11-12 21:23:19 +0100

    Done

--- module00Encapsulation/ex01/main.cpp
+++ module00Encapsulation/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hnaciri- <hnaciri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 19:52:39 by hnaciri-          #+#    #+#             */
-/*   Updated: 2023/11/12 20:34:37 by hnaciri-         ###   ########.fr       */
+/*   Updated: 2023/11/12 21:21:24 by hnaciri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,24 @@
 
 int     main()
 {
-	Graph a(5);
-	a.createVector2(0, 0);
-	a.createVector2(2, 2);
-	a.createVector2(4, 2);
-	a.createVector2(2, 4);
-	std::cout << a;
+	{
+		Graph firstGraph(5);
+		firstGraph.createVector2(0.2, 0.4);
+		firstGraph.createVector2(1.7, 1.9);
+		firstGraph.createVector2(4.1, 2.2);
+		firstGraph.createVector2(1.8, 3.7);
+		std::cout << firstGraph;
+	}
+	{
+		srand(time(0));
+		Graph secondGraph(20);
+		for (int i = 0; i < 30; i++) {
+			try {
+				secondGraph.createVector2(rand() % 25, rand() % 25);
+			}
+			catch (const std::exception& e) {
+				std::cout << e.what() << std::endl;
+			}
+		}
+	}
 }
