0ca9a67038c44f2458d58e19245f89ed6533100e
hnaciri-1337 <haithamnaciribensaghir@gmail.com>
2024-01-13 16:40:08 +0100

    Start 03

---
+++ module03SOLID/ex00/Transmission.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Transmission.hpp                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnaciri- <hnaciri-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/13 12:45:58 by hnaciri-          #+#    #+#             */
+/*   Updated: 2024/01/13 12:46:41 by hnaciri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TRANSMISSION_HPP
+#define TRANSMISSION_HPP
+
+class Transmission
+{
+public:
+	Transmission(/* args */);
+	~Transmission();
+};
+
+Transmission::Transmission(/* args */)
+{
+}
+
+Transmission::~Transmission()
+{
+}
+
+
+#endif
