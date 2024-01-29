9dfb55b63706a8ecec9ceb26ec612cf4f3fe12b3
Haitham Naciri ben saghir <hnaciri-@e1r13p2.1337.ma>
2024-01-29 12:30:09 +0100

    SOLID

---
+++ module03SOLID/ex03/EmployeeManagement.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   EmployeeManagement.hpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnaciri- <hnaciri-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 17:51:57 by hnaciri-          #+#    #+#             */
+/*   Updated: 2024/01/19 18:19:30 by hnaciri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EMPLOYEE_MANAGEMENT_HPP
+#define EMPLOYEE_MANAGEMENT_HPP
+#include <set>
+#include <iostream>
+
+class	Employee;
+
+class EmployeeManager
+{
+	private:
+		std::set<Employee *>	employees;
+	public:
+		EmployeeManager() {}
+		void	addEmployee(Employee *e) {
+			if (e)
+				employees.insert(e);
+		}
+		void	removeEmployee(Employee *e) {
+			if (e)
+				employees.erase(e);
+		}
+		void	executeWorkday() {
+			for (std::set<Employee *>::iterator it = employees.begin(); it != employees.end(); it++)
+				it->executeWorkday();
+		}
+		~EmployeeManager() {}
+};
+
+#endif
