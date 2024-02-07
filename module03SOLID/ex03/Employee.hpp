1f1bebd7548ee222d174b2674cde8014d48afa01
hnaciri-1337 <haithamnaciribensaghir@gmail.com>
2024-02-07 14:50:15 +0100

    0

---
+++ module03SOLID/ex03/Employee.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Employee.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hnaciri- <hnaciri-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 17:30:09 by hnaciri-          #+#    #+#             */
+/*   Updated: 2024/01/29 17:32:01 by hnaciri-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EMPLOYEE_HPP
+#define EMPLOYEE_HPP
+
+#include <set>
+#include <iostream>
+
+class Employee
+{
+	private:
+		int	hourlyValue;
+	public:
+		Employee() {}
+		int	executeWorkDay() {
+			
+		}
+		~Employee() {}
+};
+
+#endif
--- module03SOLID/ex03/EmployeeManagement.hpp
+++ module03SOLID/ex03/EmployeeManagement.hpp
@@ -6,7 +6,7 @@
 /*   By: hnaciri- <hnaciri-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 17:51:57 by hnaciri-          #+#    #+#             */
-/*   Updated: 2024/01/19 18:19:30 by hnaciri-         ###   ########.fr       */
+/*   Updated: 2024/01/29 17:29:49 by hnaciri-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,9 @@ class EmployeeManager
 		void	executeWorkday() {
 			for (std::set<Employee *>::iterator it = employees.begin(); it != employees.end(); it++)
 				it->executeWorkday();
+		}
+		void	calculatePayroll() {
+			
 		}
 		~EmployeeManager() {}
 };
